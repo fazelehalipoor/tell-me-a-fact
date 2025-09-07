3698de72d11c8244db9257fe0fd980c3f4b9c011
fazelehalipoor <148051136+fazelehalipoor@users.noreply.github.com>
2025-09-07 15:55:37 +0330

    Improving UX experience

--- src/script.js
+++ src/script.js
@@ -12,12 +12,16 @@ function displayFact(response) {
 function generateFact(event) {
   event.preventDefault();
 
-  let factElement = document.querySelector("#topic-input");
+  let instructionsInput = document.querySelector("#topic-input");
   let apiKey = "21b4c3ea35c50f8b3156odtae63b894e";
-  let prompt = "generate a fun fact";
-  let context = `imagine You are a precise and knowledgeable individual and you want to tell the least heard and unexpected facts when i write:${factElement.value}.answer it in less then 7 line's and two languages,English first,then in persian.Keep the two languages CLEARLY separated in different LINE'S.Do not add anything else.`;
+  let prompt = `User instructions: Generate an interesting fact about ${instructionsInput.value}`;
+  let context =
+    "You are a precise and knowledgeable persone who wants to tell the least heard and unexpected facts. Your mission is to generate a fact in basic HTML and separate each line with a <br />.write in two languages,English first,then in persian.Keep the two languages CLEARLY separated.Do NOT add anything else like html but the fact.Make sure to follow the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let factElement = document.querySelector("#generated-fact");
+  factElement.classList.remove("hidden");
+  factElement.innerHTML = `<div class="generating">⏳ Generating a Fact about ${instructionsInput.value}</div>`;
   axios.get(apiUrl).then(displayFact);
 }
 
